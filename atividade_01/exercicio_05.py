b15d1402580e04526f2de8d03f7449c4cfbff11a
Abynadark Souza <abynadarks@gmail.com>
2025-08-31 22:21:17 -0300

    Commit da Atividade 02

---
+++ atividade_01/exercicio_05.py
@@ -0,0 +1,10 @@
+A = int(input("Digite o primeiro valor:"))
+B = int(input("Digite o segundo valor:"))
+C = int(input("Digite o terceiro valor:"))
+D = int(input("Digite o quarto valor:"))
+
+# a, b, c, d = input("Digite quatro valores inteiros separados por espaço: ").split()
+
+diferenca = (A * B - C * D)
+
+print(f"DIFERENCA = {diferenca}")
