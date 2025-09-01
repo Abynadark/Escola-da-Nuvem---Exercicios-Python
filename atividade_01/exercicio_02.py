95cf41b790c03d2a1673546eb25c42d33375e65f
Abynadark Souza <abynadarks@gmail.com>
2025-08-31 22:21:17 -0300

    Commit da Atividade 01

---
+++ atividade_01/exercicio_02.py
@@ -0,0 +1,6 @@
+numero1 = 12
+nummero2 = 14
+
+soma = numero1 + nummero2 
+
+print(f"O resultado da soma é {soma}")
---
+++ atividade_01/exercicio_04.py
@@ -0,0 +1,7 @@
+nome_produto = "Cadeira Infantil"
+preco_unitario = 12.40
+quantidade = 3
+
+preco_total = preco_unitario * quantidade
+
+print(f"O valor total da compra de {quantidade} {nome_produto} de valor unitário R${preco_unitario} é igual a R${preco_total}")
